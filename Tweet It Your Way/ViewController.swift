a24c43eeade812520940cbbf421b4f0b310ce718
Nathan Ansel <nathan3o4@gmail.com>
2016-02-14 21:09:00 -0600

    Initial Commit

---
+++ Tweet It Your Way/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweet It Your Way
+//
+//  Created by Nathan Ansel on 2/14/16.
+//  Copyright © 2016 Nathan Ansel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
